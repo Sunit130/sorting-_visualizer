bdf774177170e1ce3c2e3907e7adfa4805ece676
Sunit130 <65279280+Sunit130@users.noreply.github.com>
2020-05-27 14:44:04 +0530

    Update bubble-sort.js

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,36 +1,7 @@
-
-// let arr = [];
-// let length = 160;
-// let w = 5;
-// let min = 5;
-// let max = 500;
-// let gap;
-// let i;
-
-// function setup(){
-//     createCanvas(800,600);
-//     frameRate(20);
-//     for(i=0;i<length;i++){
-//         arr.push(random(min,max));
-//     }
-//     gap = (width - (length * w))/2;
-//
-//     i=0;
-//
-// }
-
 function bubble_draw(){
-    // background(220);
-
-    // for(let j=0;j<length;j++){
-    //     fill(255);
-    //     strokeWeight(0.25)
-    //     rect(gap + j*w, height - arr[j], w, arr[j]);
-    // }
+    
     fill(color('#6af7ec'));
     rect(gap + (0)*w, height - arr[0], w, arr[0]);
-
-
 
     for(let j=0;j< length -i-1; j++){
         fill(color('#6af7ec'));
